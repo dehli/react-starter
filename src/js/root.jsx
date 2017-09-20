ff2737dfd02331a6ad558657a47d441793ce76c6
Christian Paul Dehli <dehli@gatech.edu>
2017-09-20 14:25:49 -0400

    initial commit

---
+++ src/js/root.jsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+// TODO:
+// Linting (prettifier)
+// Routing
+// Minification
+// Babel
+// Flow
+// TDD
+
+ReactDOM.render(
+  <h1>Hello, world!</h1>,
+  document.getElementById('root')
+);
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+const BUILD_DIR = path.resolve(__dirname, 'build');
+const APP_DIR = path.resolve(__dirname, 'src');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './src/index.html',
+  filename: 'index.html',
+  inject: 'body'
+});
+
+module.exports = {
+    entry: `${APP_DIR}/js/root.jsx`,
+    output: {
+        path: BUILD_DIR,
+        filename: 'bundle.js'
+    },
+    module: {
+      loaders: [
+        { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
+      ]
+    },
+    plugins: [HtmlWebpackPluginConfig]
+};
